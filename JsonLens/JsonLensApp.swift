632e496c918cc5063228c02d74d3fe0cbd8791a1
Johann six <six.johann@gmail.com>
2026-06-10 16:34:37 +0200

    Initial Commit

---
+++ JsonLens/JsonLensApp.swift
@@ -0,0 +1,32 @@
+//
+//  JsonLensApp.swift
+//  JsonLens
+//
+//  Created by Six Johann François hervé on 10/06/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct JsonLensApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ JsonLensTests/JsonLensTests.swift
@@ -0,0 +1,19 @@
+//
+//  JsonLensTests.swift
+//  JsonLensTests
+//
+//  Created by Six Johann François hervé on 10/06/2026.
+//
+
+import Testing
+@testable import JsonLens
+
+struct JsonLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
